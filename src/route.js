c4606bed36cdc2f28048f4dc66774044b39c2aef
flyflypeng <flyflyflypeng@gmail.com>
2026-01-21 16:10:45 +0000

    fix build error
    Signed-off-by: flyflypeng <flyflyflypeng@gmail.com>

--- src/route.js
+++ src/route.js
@@ -7,7 +7,7 @@ import xiaohongshu_user from './lib/xiaohongshu/user';
 
 const route = new Hono();
 
-let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user];
+let plugins = [xiaohongshu_user];
 
 for (let plugin of plugins) {
 	plugin.setup(route);
